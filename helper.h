d93223afe69da9c479bdca55ec233bd315f001ab
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2024-07-16 12:58:59 +0300

    helper func impl

---
+++ helper.h
@@ -0,0 +1,12 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <string>
+#include <algorithm>
+#include "constant.h"
+
+bool process( char *str );
+bool process( std::string& str );
+bool reverseComplement( std::string& sequence );
+
+#endif
