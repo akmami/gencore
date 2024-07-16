c45bee6ef883d343c82498c773186994d07efe06
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2024-07-16 15:21:40 +0300

    program mode added, out files seperated

--- args.h
+++ args.h
@@ -2,13 +2,16 @@
 #define ARGS_H
 
 #include <string>
+#include "program_mode.h"
 
 struct args {
+    program_mode mode;
     bool readCores;
-    bool writeCores;
-    std::string outfilename;
     std::string infilename1;
     std::string infilename2;
+    bool writeCores;
+    std::string outfilename1;
+    std::string outfilename2;
     int threadNumber;
     int lcpLevel;
 };
