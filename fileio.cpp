7454879357fe88b6a17beabbb436e84554eeae5e
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2024-08-22 17:20:47 +0300

    multithreading is supporting for reading cores, logging refactor

--- fileio.cpp
+++ fileio.cpp
@@ -4,15 +4,17 @@
 void save(args& arguments, std::vector<lcp::lps*>& cores) {
     std::ofstream out(arguments.outfilename, std::ios::binary);
     if (!out) {
-        std::cerr << "Error opening file for writing" << std::endl;
+        log(ERROR,  "Error opening file for writing %s", arguments.infilename.c_str());
         return;
     }
 
-    std::cout << "Saving cores to file " << arguments.outfilename << std::endl;
+    log(INFO, "Saving cores to file %s", arguments.outfilename.c_str());
 
-    size_t size = cores.size();
+    size_t core_size = cores.size();
+    size_t genome_size = arguments.size;
 
-    out.write(reinterpret_cast<const char*>(&size), sizeof(size));
+    out.write(reinterpret_cast<const char*>(&core_size), sizeof(core_size));
+    out.write(reinterpret_cast<const char*>(&genome_size), sizeof(genome_size));
 
     for ( std::vector<lcp::lps*>::iterator it = cores.begin(); it != cores.end(); it++ ) {
         (*it)->write(out);
@@ -26,19 +28,21 @@ void load(args& arguments, std::vector<lcp::lps*>& cores) {
     std::ifstream in(arguments.infilename, std::ios::binary);
 
     if (!in) {
-        std::cerr << "Error opening file for reading" << std::endl;
+        log(ERROR,  "Error opening file for reading %s", arguments.infilename.c_str());
         return;
     }
 
-    std::cout << "Loading cores from file " << arguments.infilename << std::endl;
+    log(INFO, "Loading cores from file %s", arguments.infilename.c_str());
 
-    size_t size;
+    size_t core_size, genome_size;
 
-    in.read(reinterpret_cast<char*>(&size), sizeof(size));
+    in.read(reinterpret_cast<char*>(&core_size), sizeof(core_size));
+    in.read(reinterpret_cast<char*>(&genome_size), sizeof(genome_size));
 
-    cores.reserve(size);
+    cores.reserve(core_size);
+    arguments.size = genome_size;
 
-    for ( size_t i = 0; i < size; i++ ) {
+    for ( size_t i = 0; i < core_size; i++ ) {
         lcp::lps *str = new lcp::lps(in);
         str->deepen(arguments.lcpLevel);
 
@@ -64,4 +68,33 @@ void read_from_file( args& arguments ) {
     }
 
     strs.clear();
+};
+
+
+void read_cores( std::vector<args>& arguments ) {
+
+    const size_t THREADNUMBER = arguments[0].threadNumber;
+    std::vector<std::thread> threads;
+    std::vector<args>::iterator current_argument = arguments.begin();
+
+    while (current_argument < arguments.end()) {
+
+        while (threads.size() < THREADNUMBER && current_argument < arguments.end()) {
+            threads.emplace_back(read_from_file, std::ref(*current_argument));
+            current_argument++;
+        }
+
+        for ( std::vector<std::thread>::iterator it = threads.begin(); it != threads.end(); ) {
+            if (it->joinable()) {
+                it->join();
+                it = threads.erase(it);
+            } else {
+                it++;
+            }
+        }
+    }
+
+    for (std::vector<std::thread>::iterator it = threads.begin(); it != threads.end(); it++ ) {
+        it->join();
+    }
 };
--- fileio.h
+++ fileio.h
@@ -3,13 +3,16 @@
 
 #include <vector>
 #include <string>
+#include <thread>
 #include "args.h"
 #include "lps.h"
 #include "helper.h"
 #include "similarity_metrics.h"
+#include "logging.h"
 
 void save(args& arguments, std::vector<lcp::lps*>& cores);
 void load(args& arguments, std::vector<lcp::lps*>& cores);
 void read_from_file(args& arguments);
+void read_cores(std::vector<args>& arguments);
 
 #endif
