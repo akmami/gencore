75b18a670f760993ec18552a3c326e978845f10b
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2024-09-30 12:01:30 +0300

    docs and logs added, new arguments integrated

--- fileio.cpp
+++ fileio.cpp
@@ -1,14 +1,14 @@
 #include "fileio.h"
 
 
-void save(args& arguments, std::vector<lcp::lps*>& cores) {
-    std::ofstream out(arguments.outfilename, std::ios::binary);
+void save( const struct targs& arguments, const std::vector<lcp::lps*>& cores ) {
+    std::ofstream out(arguments.outFileName, std::ios::binary);
     if (!out) {
-        log(ERROR,  "Error opening file for writing %s", arguments.infilename.c_str());
+        log(ERROR, "Error opening file for writing %s", arguments.inFileName.c_str());
         return;
     }
 
-    log(INFO, "Saving cores to file %s", arguments.outfilename.c_str());
+    log(INFO, "Saving cores to file %s", arguments.outFileName.c_str());
 
     size_t core_size = cores.size();
     size_t genome_size = arguments.size;
@@ -16,7 +16,7 @@ void save(args& arguments, std::vector<lcp::lps*>& cores) {
     out.write(reinterpret_cast<const char*>(&core_size), sizeof(core_size));
     out.write(reinterpret_cast<const char*>(&genome_size), sizeof(genome_size));
 
-    for ( std::vector<lcp::lps*>::iterator it = cores.begin(); it != cores.end(); it++ ) {
+    for ( std::vector<lcp::lps*>::const_iterator it = cores.begin(); it != cores.end(); it++ ) {
         (*it)->write(out);
     }
 
@@ -24,27 +24,25 @@ void save(args& arguments, std::vector<lcp::lps*>& cores) {
 };
 
 
-void load(args& arguments, std::vector<lcp::lps*>& cores) {
-    std::ifstream in(arguments.infilename, std::ios::binary);
+void load( struct targs& thread_arguments, struct pargs& program_arguments, std::vector<lcp::lps*>& cores ) {
+    std::ifstream in(thread_arguments.inFileName, std::ios::binary);
 
     if (!in) {
-        log(ERROR,  "Error opening file for reading %s", arguments.infilename.c_str());
+        log(ERROR, "Error opening file for reading %s", thread_arguments.inFileName.c_str());
         return;
     }
-
-    log(INFO, "Loading cores from file %s", arguments.infilename.c_str());
-
+    
     size_t core_size, genome_size;
 
     in.read(reinterpret_cast<char*>(&core_size), sizeof(core_size));
     in.read(reinterpret_cast<char*>(&genome_size), sizeof(genome_size));
 
     cores.reserve(core_size);
-    arguments.size = genome_size;
+    thread_arguments.size = genome_size;
 
     for ( size_t i = 0; i < core_size; i++ ) {
         lcp::lps *str = new lcp::lps(in);
-        str->deepen(arguments.lcpLevel);
+        str->deepen(program_arguments.lcpLevel);
 
         cores.push_back(str);
     }
@@ -53,34 +51,47 @@ void load(args& arguments, std::vector<lcp::lps*>& cores) {
 };
 
 
-void read_from_file( args& arguments ) {
+void read_from_file( struct targs& thread_arguments, struct pargs& program_arguments ) {
 
+    // get thread id
+    std::ostringstream ss;
+    ss << std::this_thread::get_id();
+
+    // log initiation of reading fasta
+    log(INFO, "Thread ID: %s started loading %s", ss.str().c_str(), thread_arguments.inFileName.c_str());
+
+    // load lcp cores
     std::vector<lcp::lps*> strs;
+    load( thread_arguments, program_arguments, strs );
 
-    load( arguments, strs );
+    // log ending of processing fasta
+    log(INFO, "Thread ID: %s ended loading %s", ss.str().c_str(), thread_arguments.inFileName.c_str());
 
-    flatten( strs, arguments.lcp_cores );
+    // get lcp core hashes
+    std::vector<uint32_t> lcp_core_hashes;
+    flatten(strs, lcp_core_hashes);
 
-    generateMinhashSignature(arguments.lcp_cores);
-
+    // delete lcp cores
     for ( std::vector<lcp::lps*>::iterator it = strs.begin(); it != strs.end(); it++ ) {
         delete (*it);
     }
-
     strs.clear();
+
+    // set lcp cores and counts to arguments
+    generateSignature( lcp_core_hashes );
+    initializeSetAndCounts( lcp_core_hashes, thread_arguments.cores, thread_arguments.counts );
 };
 
 
-void read_cores( std::vector<args>& arguments ) {
+void read_cores( std::vector<struct targs>& thread_arguments, struct pargs& program_arguments ) {
 
-    const size_t THREADNUMBER = arguments[0].threadNumber;
     std::vector<std::thread> threads;
-    std::vector<args>::iterator current_argument = arguments.begin();
+    std::vector<struct targs>::iterator current_argument = thread_arguments.begin();
 
-    while (current_argument < arguments.end()) {
+    while (current_argument < thread_arguments.end()) {
 
-        while (threads.size() < THREADNUMBER && current_argument < arguments.end()) {
-            threads.emplace_back(read_from_file, std::ref(*current_argument));
+        while (threads.size() < program_arguments.threadNumber && current_argument < thread_arguments.end()) {
+            threads.emplace_back(read_from_file, std::ref(*current_argument), std::ref(program_arguments));
             current_argument++;
         }
 
--- fileio.h
+++ fileio.h
@@ -4,15 +4,73 @@
 #include <vector>
 #include <string>
 #include <thread>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 #include "args.h"
 #include "lps.h"
 #include "helper.h"
-#include "similarity_metrics.h"
 #include "logging.h"
 
-void save(args& arguments, std::vector<lcp::lps*>& cores);
-void load(args& arguments, std::vector<lcp::lps*>& cores);
-void read_from_file(args& arguments);
-void read_cores(std::vector<args>& arguments);
+
+/**
+ * @brief Saves the LCP cores to a binary file.
+ * 
+ * This function writes the contents of the provided LCP cores to a binary file specified 
+ * by the `outFileName` in the `arguments` structure. It also records metadata about the 
+ * number of cores and the total size of the genome being processed.
+ * 
+ * @param arguments A constant reference to the `targs` structure, which contains file output 
+ *        information (such as the output file name) and the total size of the processed genome.
+ * @param cores A constant reference to a vector of pointers to `lcp::lps` objects, representing
+ *        the LCP cores that will be saved to the file.
+ */
+void save( const struct targs& arguments, const std::vector<lcp::lps*>& cores );
+
+/**
+ * @brief Loads LCP cores from a binary file into a vector.
+ * 
+ * This function reads the specified binary file to load LCP core data into a provided vector of 
+ * `lcp::lps` pointers. It extracts the total number of cores and the associated genome size from 
+ * the file, which are then stored in the respective fields of the `thread_arguments`.
+ * 
+ * @param thread_arguments A reference to a `targs` structure that contains the input file name and
+ *        will be updated with the total genome size after loading the cores.
+ * @param program_arguments A reference to a `pargs` structure that contains program-wide settings, 
+ *        including the level of LCP depth to be applied to each loaded core.
+ * @param cores A reference to a vector of pointers to `lcp::lps` objects, where the loaded LCP cores 
+ *        will be stored.
+ */
+void load( struct targs& arguments, struct pargs& program_arguments, std::vector<lcp::lps*>& cores );
+
+/**
+ * @brief Reads LCP cores from a file and processes them.
+ * 
+ * This function loads LCP (Longest Common Prefix) cores from a specified file and extracts their 
+ * hashes for further processing. It manages memory by deleting loaded LCP core objects after 
+ * extracting their hashes and setting the results into the provided `thread_arguments`.
+ * 
+ * @param thread_arguments A reference to a `targs` structure containing file-specific data (e.g., input file name) 
+ *        and will be updated with the extracted LCP cores and their counts.
+ * @param program_arguments A reference to a `pargs` structure containing program-wide settings needed 
+ *        for loading and processing the LCP cores.
+ */
+void read_from_file( struct targs& thread_arguments, struct pargs& program_arguments );
+
+/**
+ * @brief Reads core data from multiple files using multithreading.
+ * 
+ * This function spawns multiple threads to concurrently read core data from files specified in the 
+ * `thread_arguments` structure. The number of threads spawned is controlled by the `threadNumber` 
+ * parameter in the `program_arguments` structure. Once the reading tasks are completed, the threads are 
+ * joined and cleaned up.
+ * 
+ * @param thread_arguments A reference to a vector of `targs` structures, where each element contains 
+ *        file information (e.g., input file names) and is passed to the respective threads for reading.
+ * @param program_arguments A reference to the `pargs` structure, which contains general program settings, 
+ *        including the number of threads to spawn (`threadNumber`).
+ */
+void read_cores( std::vector<struct targs>& thread_arguments, struct pargs& program_arguments );
+
 
 #endif
