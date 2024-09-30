19a7ab209199b94f704695f96bb28d25b3be8941
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2024-09-30 12:08:47 +0300

    code refactor: integrated new args and docs added

--- rfasta.cpp
+++ rfasta.cpp
@@ -1,15 +1,15 @@
 #include "rfasta.h"
 
-void read_fastas( std::vector<args>& arguments ) {
+
+void read_fastas( std::vector<struct targs>& thread_arguments, const struct pargs& program_arguments ) {
     
-    const size_t THREADNUMBER = arguments[0].threadNumber;
     std::vector<std::thread> threads;
-    std::vector<args>::iterator current_argument = arguments.begin();
+    std::vector<struct targs>::iterator current_argument = thread_arguments.begin();
 
-    while (current_argument < arguments.end()) {
+    while (current_argument < thread_arguments.end()) {
 
-        while (threads.size() < THREADNUMBER && current_argument < arguments.end()) {
-            threads.emplace_back(read_fasta, std::ref(*current_argument));
+        while (threads.size() < program_arguments.threadNumber && current_argument < thread_arguments.end()) {
+            threads.emplace_back(read_fasta, std::ref(*current_argument), std::ref(program_arguments));
             current_argument++;
         }
 
@@ -29,70 +29,105 @@ void read_fastas( std::vector<args>& arguments ) {
 };
 
 
-void read_fasta( args& arguments ) {
+void read_fasta( struct targs& thread_arguments, const struct pargs& program_arguments ) {
+    
+    // get thread id
     std::ostringstream ss;
     ss << std::this_thread::get_id();
-    log(INFO, "Thread ID: %s started processing %s", ss.str().c_str(), arguments.infilename.c_str());
 
+    // log initiation of reading fasta
+    log(INFO, "Thread ID: %s started processing %s", ss.str().c_str(), thread_arguments.inFileName.c_str());
+
+    // open fasta file
     std::fstream file;
-    file.open( arguments.infilename, std::ios::in );
-
-    int chrom_index = 0;
-    std::string line;
-    size_t genome_size = 0;
+    file.open( thread_arguments.inFileName, std::ios::in );
 
     std::vector<lcp::lps*> strs;
+    thread_arguments.size = 0;
 
     // read file
     if ( file.is_open() ) {  
-
-        std::string sequence, id;
+        
+        
+        std::string sequence, id, line;
         sequence.reserve(250000000);
         
         while (getline(file, line)) {
 
             if (line[0] == '>') {
 
-                chrom_index++;
-
                 // process previous chromosome before moving into new one
                 if (sequence.size() != 0) {
                 
                     lcp::lps* str = new lcp::lps(sequence);
-                    str->deepen(arguments.lcpLevel);
-
+                    str->deepen(program_arguments.lcpLevel);
                     strs.push_back(str);
-                    genome_size += sequence.size();
-                    
+
+                    // increment processed sequence size
+                    thread_arguments.size += sequence.size();
+
+                    if ( program_arguments.verbose ) {
+                        log(INFO, "Thread ID: %s, Length of the processed sequence: %d", ss.str().c_str(), sequence.size());
+                    }
+
+                    // cleanup
                     sequence.clear();
                 }
                 
+                // get new chromosome's id
                 id = line.substr(1);
-                continue;
+                
+                if ( program_arguments.verbose ) {
+                    log(INFO, "Thread ID: %s, Processing started for %s", ss.str().c_str(), id.c_str());
+                }
             }
             else if (line[0] != '>'){
                 sequence += line;
             }
         }
+
+        // process last chromosome set into sequence string
+        if ( sequence.size() != 0 ) {
+            lcp::lps* str = new lcp::lps(sequence);
+            str->deepen(program_arguments.lcpLevel);
+            strs.push_back(str);
+
+            // increment processed sequence size
+            thread_arguments.size += sequence.size();
+
+            if ( program_arguments.verbose ) {
+                log(INFO, "Thread ID: %s, Length of the processed sequence: %d", ss.str().c_str(), sequence.size());
+            }
+
+            // cleanup
+            sequence.clear();
+        }
         
-        arguments.size = genome_size;
         file.close();
     } else {
-        log(ERROR, "Could not be able to open %s", arguments.infilename.c_str());
+        log(ERROR, "Could not be able to open %s", thread_arguments.inFileName.c_str());
         exit(1);
     }
-    
-    flatten(strs, arguments.lcp_cores);
 
-    if ( arguments.writeCores ) {
-        save( arguments, strs );
+    // log ending of processing fasta
+    log(INFO, "Thread ID: %s ended processing %s", ss.str().c_str(), thread_arguments.inFileName.c_str());
+
+    // write cores to file if user specified to do so
+    if ( program_arguments.writeCores ) {
+        save( thread_arguments, strs );
     }
 
+    // get lcp core hashes
+    std::vector<uint32_t> lcp_core_hashes;
+    flatten(strs, lcp_core_hashes);
+
+    // delete lcp cores
     for ( std::vector<lcp::lps*>::iterator it = strs.begin(); it != strs.end(); it++ ) {
         delete (*it);
     }
-
     strs.clear();
 
-    generateMinhashSignature(arguments.lcp_cores);
+    // set lcp cores and counts to arguments
+    generateSignature( lcp_core_hashes );
+    initializeSetAndCounts( lcp_core_hashes, thread_arguments.cores, thread_arguments.counts );
 };
--- rfasta.h
+++ rfasta.h
@@ -1,6 +1,7 @@
 #ifndef RFASTA_H
 #define RFASTA_H
 
+#include <cstdint>
 #include <string>
 #include <vector>
 #include <fstream>
@@ -8,13 +9,71 @@
 #include <sstream>
 #include <thread>
 #include "args.h"
-#include "lps.h"
-#include "helper.h"
-#include "fileio.h"
-#include "similarity_metrics.h"
 #include "logging.h"
+#include "helper.h"
+#include "lps.h"
+#include "fileio.h"
 
-void read_fastas( std::vector<args>& arguments );
-void read_fasta( args& arguments );
+
+/**
+ * @brief Reads multiple FASTA files concurrently using a pool of threads.
+ * 
+ * This function processes a collection of FASTA files by spawning threads 
+ * based on the thread arguments (`targs`) and program settings (`pargs`). It 
+ * manages the number of concurrent threads and ensures that each thread reads 
+ * a FASTA file using the `read_fasta` function, which operates on the provided 
+ * thread arguments and shared program settings. 
+ * 
+ * @param thread_arguments A reference to a vector of `targs` structures 
+ *        representing the arguments specific to each thread.
+ * @param program_arguments A constant reference to a `pargs` structure 
+ *        representing the global program arguments.
+ * 
+ * @details 
+ * - The function uses a pool of threads, limiting the number of active threads 
+ *   based on `program_arguments.threadNumber`. 
+ * - Threads are launched until the maximum thread limit is reached, and once a 
+ *   thread finishes its work, it is joined and removed from the active thread pool.
+ * - The function continues to launch new threads until all FASTA files in 
+ *   `thread_arguments` have been processed.
+ * - Once all threads are launched, the function ensures that all threads are 
+ *   joined before exiting, preventing any orphan threads from continuing execution.
+ */
+void read_fastas( std::vector<struct targs>& thread_arguments, const struct pargs& program_arguments );
+
+/**
+ * @brief Reads a FASTA file and processes its sequences using the LCP (Locally Consistent Parsing) method.
+ * 
+ * This function is responsible for reading a FASTA file specified in the `thread_arguments`, 
+ * processing each sequence using LCP techniques, and storing the results. The function manages 
+ * logging for verbose output, tracks the size of processed sequences, and handles thread-safe 
+ * operations, as it is designed to be run in a multithreaded environment.
+ * 
+ * @param thread_arguments A reference to the `targs` structure that contains the thread-specific 
+ *        arguments, including the input FASTA file name, the output data structures (`cores`, 
+ *        `counts`), and size tracking.
+ * @param program_arguments A constant reference to the `pargs` structure that contains the 
+ *        program-wide settings, such as the LCP depth level (`lcpLevel`), verbosity, and whether 
+ *        to write LCP cores to file.
+ * 
+ * @details
+ * - The function opens the FASTA file specified in `thread_arguments.inFileName` and processes 
+ *   each chromosome or sequence individually.
+ * - For each sequence, an `lps` (locally parsed string) object is created, and its depth is increased using 
+ *   `deepen()`. The sequence is then stored in a vector for further processing.
+ * - If the `verbose` flag in `program_arguments` is set, the function logs detailed information about each 
+ *   sequence, including its ID and size.
+ * - Once all sequences are processed, the function optionally saves the LCP cores to a file if the `writeCores` 
+ *   flag is enabled.
+ * - Finally, the LCP cores are flattened and processed to generate a signature and initialize sets and counts.
+ * - The function ensures proper memory management by cleaning up dynamically allocated `lps` objects.
+ * 
+ * @note This function is designed to be run in a multithreaded environment, with each thread handling a 
+ *       separate FASTA file or sequence set. The function logs the initiation and completion of each thread 
+ *       using the thread ID for traceability.
+ * 
+ * @see flatten(), generateSignature(), initializeSetAndCounts(), save(), log()
+ */
+void read_fasta( struct targs& thread_arguments, const struct pargs& program_arguments );
 
 #endif
